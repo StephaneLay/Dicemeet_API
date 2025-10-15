f7613bd9b54661967c90042b7496b8e35f6167ad
StephaneLay <stephane.lay@hotmail.fr>
2025-10-15 19:02:06 +0200

    meetup update

--- src/Controller/MeetupController.php
+++ src/Controller/MeetupController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\MeetupRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,36 +20,58 @@ final class MeetupController extends AbstractController
        return $this->json($events, 200 );
     }
 
+    #[Route('/api/public/events/{id}', name: 'app_meetup_detail' , methods: ['GET'])]
+    public function getEventById(MeetupRepository $meetupRepository, int $id): Response
+    {
+        $event = $meetupRepository->find($id);
 
-#[Route('/api/public/events/search', name: 'app_meetup_search', methods: ['GET','OPTIONS'])]
-public function search(Request $request, MeetupRepository $meetupRepository): Response
-{
-    $filters = $request->query->get('filters', '');
-    $filterArray = explode(',', $filters);
-
-    $cityNames = [];
-    $gameNames = [];
-    $barNames  = [];
-
-    foreach ($filterArray as $filter) {
-        [$type, $name] = explode(':', $filter);
-
-        switch ($type) {
-            case 'ville':
-                $cityNames[] = $name;
-                break;
-            case 'jeu':
-                $gameNames[] = $name;
-                break;
-            case 'bar':
-                $barNames[] = $name;
-                break;
+        if (!$event) {
+            return $this->json(['error' => 'Event not found'], 404);
         }
+
+        return $this->json($event, 200);
     }
 
-    $events = $meetupRepository->findByFilters($cityNames, $gameNames, $barNames);
 
-    return $this->json($events, 200);
-}
+    #[Route('/api/public/events/search', name: 'app_meetup_search', methods: ['GET','OPTIONS'])]
+    public function search(Request $request, MeetupRepository $meetupRepository): Response
+    {
+        $filters = $request->query->get('filters', '');
+        $filterArray = explode(',', $filters);
+
+        $cityNames = [];
+        $gameNames = [];
+        $barNames  = [];
+
+        foreach ($filterArray as $filter) {
+            [$type, $name] = explode(':', $filter);
+
+            switch($type) {
+                case 'ville':
+                    $cityNames[] = $name;
+                    break;
+                case 'jeu':
+                    $gameNames[] = $name;
+                    break;
+                case 'bar':
+                    $barNames[] = $name;
+                    break;
+            }
+        }
+
+        $events = $meetupRepository->findByFilters($cityNames, $gameNames, $barNames);
+
+        return $this->json($events, 200);
+    }
+
+    #[Route('/api/public/events/users/{userId}', name: 'app_meetup_search', methods: ['GET','OPTIONS'])]
+    public function getUserEvents(UserRepository $userRepository,MeetupRepository $meetupRepository, int $userId): Response
+    {
+        $user = $userRepository->findOneBy(['id'=> $userId]);
+        $events = $user->getMeetups();
+
+        return $this->json($events, 200);
+    }
+
 
 }
